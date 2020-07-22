a4d28d5ded5fbb69981dd4c2cfe234c61a76d759
phongcoiydbg99 <phongcoiydbg99@gmail.com>
2020-07-23 00:30:51 +0700

    up

--- app/src/main/java/com/example/example44/OrderActivity.java
+++ app/src/main/java/com/example/example44/OrderActivity.java
@@ -5,10 +5,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.RadioButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class OrderActivity extends AppCompatActivity {
     private TextView message;
+    private TextView mAddressText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,7 +22,37 @@ public class OrderActivity extends AppCompatActivity {
         Intent intent = getIntent();
         String orderMessage = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
 //        Log.d("Fang", orderMessage);
+        mAddressText = (TextView) findViewById(R.id.address_text);
         message = (TextView) findViewById(R.id.message_textview);
+
             message.setText(orderMessage);
+    }
+    public void displayToast(String message) {
+        Toast.makeText(getApplicationContext(), message,
+                Toast.LENGTH_SHORT).show();
+    }
+    public void onRadioButtonClicked(View view) {
+        boolean checked = ((RadioButton) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.sameday :
+                if (checked)
+                    // Same day service
+                    displayToast(mAddressText.getText().toString());
+                break;
+            case R.id.nextday:
+                if (checked)
+                    // Next day delivery
+                    displayToast(getString(R.string.next_day_ground_delivery));
+                break;
+            case R.id.pickup:
+                if (checked)
+                    // Pick up
+                    displayToast(getString(R.string.pick_up));
+                break;
+            default:
+                // Do nothing.
+                break;
+        }
     }
 }
